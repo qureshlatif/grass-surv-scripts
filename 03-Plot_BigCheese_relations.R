a760773e99f9056fd7b22d09731726b223b42faa
Quresh Latif <qureshlatif@yahoo.com>
2020-10-01 09:41:20 -0600

    more adjustments to plots before review of Methods,Results,Appendix

--- 03-Plot_BigCheese_relations.R
+++ 03-Plot_BigCheese_relations.R
@@ -95,16 +95,13 @@ dat.plot <- dat.plot.hierbas.BAIS %>%
               mutate(spp = "GRSP")) %>%
   mutate(spp = factor(spp, levels = c("BAIS", "GRSP")))
 p.hierbas.PSR <- ggplot(dat.plot, aes(x = x, y = PSR.md)) +
-  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3,
-              show.legend = F) +
+  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3) +
   geom_line(aes(color = spp), size = 1) +
   scale_color_manual(values = colors.spp) +
   scale_fill_manual(values = fill.spp) +
   scale_linetype_manual(values = linetype.spp) +
   ylim(0, 1) +
-  labs(color = "Species") +
-  theme(legend.position = c(0,1), legend.justification = c(0,1)) +
-  #guides(color = F, fill = F, linetype = F) +
+  guides(color = F, fill = F, linetype = F) +
   xlab("Forb cover 5m (%)") + ylab(NULL)
 
 # Forb height #
@@ -207,6 +204,45 @@ p.otra_cv.PSR <- ggplot(dat.plot, aes(x = x, y = PSR.md)) +
   ylim(0, 1) +
   guides(color = F, fill = F, linetype = F) +
   xlab("CV other cover 5m (%)") + ylab(NULL)
+
+# Shrub cover 5m #
+supp <- c(T, T)
+fill.spp <- ifelse(supp, colors.spp, "white")
+linetype.spp <- ifelse(supp, "solid", "dashed")
+dat.plot <- dat.plot.Shrub_All_5m.BAIS %>%
+  mutate(spp = "BAIS") %>%
+  bind_rows(dat.plot.Shrub_All_5m.GRSP %>%
+              mutate(spp = "GRSP")) %>%
+  mutate(spp = factor(spp, levels = c("BAIS", "GRSP"))) %>%
+  mutate(x = x * 100)
+p.Shrub_All_5m.PSR <- ggplot(dat.plot, aes(x = x, y = PSR.md)) +
+  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3) +
+  geom_line(aes(color = spp), size = 1) +
+  scale_color_manual(values = colors.spp) +
+  scale_fill_manual(values = fill.spp) +
+  scale_linetype_manual(values = linetype.spp) +
+  ylim(0, 1) +
+  guides(color = F, fill = F, linetype = F) +
+  xlab("Shrub cover 5m (%)") + ylab(NULL)
+
+# Shrub height 5m #
+supp <- c(T, F)
+fill.spp <- ifelse(supp, colors.spp, "white")
+linetype.spp <- ifelse(supp, "solid", "dashed")
+dat.plot <- dat.plot.Max_Shrub_Height_5m.BAIS %>%
+  mutate(spp = "BAIS") %>%
+  bind_rows(dat.plot.Max_Shrub_Height_5m.GRSP %>%
+              mutate(spp = "GRSP")) %>%
+  mutate(spp = factor(spp, levels = c("BAIS", "GRSP")))
+p.Max_Shrub_Height_5m.PSR <- ggplot(dat.plot, aes(x = x, y = PSR.md)) +
+  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3) +
+  geom_line(aes(color = spp), size = 1) +
+  scale_color_manual(values = colors.spp) +
+  scale_fill_manual(values = fill.spp) +
+  scale_linetype_manual(values = linetype.spp) +
+  ylim(0, 1) +
+  guides(color = F, fill = F, linetype = F) +
+  xlab("Shrub height 5m (m)") + ylab(NULL)
 
 # Shrub cover 5m CV #
 supp <- c(T, T)
@@ -335,38 +371,44 @@ dat.plot <- dat.plot.raptor.BAIS %>%
               mutate(spp = "GRSP")) %>%
   mutate(spp = factor(spp, levels = c("BAIS", "GRSP")))
 p.raptor.PSR <- ggplot(dat.plot, aes(x = x, y = PSR.md)) +
-  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3) +
+  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = spp, color = spp, linetype = spp), alpha = 0.3,
+              show.legend = F) +
   geom_line(aes(color = spp), size = 1) +
   scale_color_manual(values = colors.spp) +
   scale_fill_manual(values = fill.spp) +
   scale_linetype_manual(values = linetype.spp) +
   ylim(0, 1) +
-  guides(color = F, fill = F, linetype = F) +
+  labs(color = "Species") +
+  #theme(legend.position = c(0,1), legend.justification = c(0,1)) +
+  #guides(color = F, fill = F, linetype = F) +
   xlab(expression('Raptor density (per '*km^2*')')) + ylab(NULL)
 
 # Put everything together
 p <- ggdraw() +
-  draw_plot(p.hierbas.PSR,           x = 0,    y = 0.6667, width = 0.25, height = 0.3333) +
-  draw_plot(p.hierba_ht.PSR,         x = 0.25, y = 0.6667, width = 0.25, height = 0.3333) +
-  draw_plot(p.hierbas_cv.PSR,        x = 0.5,  y = 0.6667, width = 0.25, height = 0.3333) +
-  draw_plot(p.pasto_ht.PSR,          x = 0.75, y = 0.6667, width = 0.25, height = 0.3333) +
+  draw_plot(p.hierbas.PSR,             x = 0,    y = 0.75, width = 0.25, height = 0.25) +
+  draw_plot(p.hierba_ht.PSR,           x = 0.25, y = 0.75, width = 0.25, height = 0.25) +
+  draw_plot(p.hierbas_cv.PSR,          x = 0.5,  y = 0.75, width = 0.25, height = 0.25) +
+  draw_plot(p.pasto_ht.PSR,            x = 0.75, y = 0.75, width = 0.25, height = 0.25) +
   
-  draw_plot(p.otra.PSR,              x = 0,    y = 0.3333, width = 0.25, height = 0.3333) +
-  draw_plot(p.otra_cv.PSR,           x = 0.25, y = 0.3333, width = 0.25, height = 0.3333) +
-  draw_plot(p.Shrub_All_5m_CV.PSR,   x = 0.5,  y = 0.3333, width = 0.25, height = 0.3333) +
-  draw_plot(p.Shrub_All_500m_CV.PSR, x = 0.75, y = 0.3333, width = 0.25, height = 0.1665) +
-  draw_plot(p.Shrub_All_50m_CV.PSR,  x = 0.75, y = 0.4995, width = 0.25, height = 0.1665) +
+  draw_plot(p.otra.PSR,                x = 0,    y = 0.5,  width = 0.25, height = 0.25) +
+  draw_plot(p.otra_cv.PSR,             x = 0.25, y = 0.5,  width = 0.25, height = 0.25) +
+  draw_plot(p.Shrub_All_5m.PSR,        x = 0.5,  y = 0.5,  width = 0.25, height = 0.25) +
+  draw_plot(p.Max_Shrub_Height_5m.PSR, x = 0.75, y = 0.5,  width = 0.25, height = 0.25) +
   
-  draw_plot(p.Distance_to_Fence.PSR, x = 0,    y = 0,      width = 0.25, height = 0.3333) +
-  draw_plot(p.peso.PSR,              x = 0.25, y = 0,      width = 0.25, height = 0.3333) +
-  draw_plot(p.prey.PSR,              x = 0.5,  y = 0,      width = 0.25, height = 0.3333) +
-  draw_plot(p.raptor.PSR,            x = 0.75, y = 0,      width = 0.25, height = 0.3333)
+  draw_plot(p.Shrub_All_5m_CV.PSR,     x = 0,    y = 0.25, width = 0.25, height = 0.25) +
+  draw_plot(p.Shrub_All_500m_CV.PSR,   x = 0.25, y = 0.25, width = 0.25, height = 0.25) +
+  draw_plot(p.Shrub_All_50m_CV.PSR,    x = 0.5,  y = 0.25, width = 0.25, height = 0.25) +
+  draw_plot(p.Distance_to_Fence.PSR,   x = 0.75, y = 0.25, width = 0.25, height = 0.25) +
+  
+  draw_plot(p.peso.PSR,                x = 0,    y = 0,    width = 0.25, height = 0.25) +
+  draw_plot(p.prey.PSR,                x = 0.25, y = 0,    width = 0.25, height = 0.25) +
+  draw_plot(p.raptor.PSR,              x = 0.5,  y = 0,    width = 0.35,  height = 0.25)
 p <- ggdraw() +
   draw_plot(p, x = 0.05, y = 0, width = 0.95, height = 1) +
   draw_plot_label("Survival over 90 days", x = 0, y = 0.4, angle = 90, hjust = 0, size = 20)
 
 #save_plot("Figure_BigCheese_additive.tiff", p, ncol = 3, nrow = 2.5, dpi = 600)
-save_plot("Figure_BigCheese_additive.jpg", p, ncol = 2.5, nrow = 2, dpi = 600)
+save_plot("Figure_BigCheese_additive.jpg", p, ncol = 2.5, nrow = 2.5, dpi = 600)
 
 # Shrub cover X height #
 # BAIS #
@@ -383,8 +425,8 @@ p.height.BAIS <- ggplot(dat.plot.shrubs, aes(x = x1, y = PSR.md)) +
   geom_line(aes(color = x2), size = 1) +
   geom_errorbar(data = dat.plot.noshrubs, aes(x = x1, ymin = PSR.lo, ymax = PSR.hi), width = 0.01) +
   geom_point(data = dat.plot.noshrubs, aes(x = x1, y = PSR.md), size = 2) +
-  scale_color_manual(labels = c("0.006", "15"), values = c("#999999", "#009E73")) +
-  scale_fill_manual(labels = c("0.006", "15"), values = c("#999999", "#009E73")) +
+  scale_color_manual(labels = c("0.006", "15"), values = c("#999999", "#D55E00")) + #009E73
+  scale_fill_manual(labels = c("0.006", "15"), values = c("#999999", "#D55E00")) + #009E73
   annotate("text", x = -0.03, y = 0.3, label = "No shrubs", size = 4, angle = 90) +
   geom_vline(xintercept = 0.055, linetype = "dashed") +
   labs(fill = "Shrub cover (%)", color = "Shrub cover (%)") + 
@@ -396,12 +438,19 @@ dat.plot <- dat.plot.Max_Shrub_Height_5mXShrub_All_5m.BAIS %>%
   mutate(precision = 1/(PSR.hi - PSR.lo)) %>%
   mutate(x2 = x2*100)
 p.heat.BAIS <- ggplot(dat.plot, aes(x1, x2)) +
-  geom_tile(aes(fill = PSR.md), color = "white") +
-  scale_fill_gradient(low = "#0072B2", high = "#D55E00") +
+  #geom_tile(aes(fill = PSR.md), color = "white") +
+  geom_raster(aes(fill = PSR.md)) +
+  geom_contour(aes(z = PSR.md),
+               color = "white", linetype = 2, size = 0.8, binwidth = 0.02) +
+  #scale_fill_gradient(low = "#0072B2", high = "#009E73", #F0E442
+  #                    breaks = seq(0, 0.14, by = 0.02)) +
+  scale_fill_gradient(low = "blue", high = "yellow", ##F0E442
+                      breaks = seq(0, 0.22, by = 0.04)) +
+  #scale_fill_gradient(colors = viridis(5)) +
   ylab("Shrub cover (%)") +
   xlab("Shrub height (m)") +
   theme(legend.title = element_text(size = 10),
-        legend.text = element_text(size = 12)) +
+        legend.text = element_text(size = 8)) +
   labs(fill = "Survival over\n90 days")
 
 # p.scatter.BAIS <- ggplot(data.BAIS$Covs %>% filter(Shrub_All_5m > 0 & Max_Shrub_Height_5m != mean(Max_Shrub_Height_5m)),
@@ -420,15 +469,14 @@ dat.plot.shrubs <- dat.plot.Max_Shrub_Height_5mXShrub_All_5m.GRSP %>%
   filter(x2 %in% c(min.shrubs, max.shrubs)) %>%
   mutate(x2 = round(x2, digits = 5) %>% as.factor)
 p.height.GRSP <- ggplot(dat.plot.shrubs, aes(x = x1, y = PSR.md)) +
-  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, fill = x2), alpha = 0.3) +
+  geom_ribbon(aes(ymin = PSR.lo, ymax = PSR.hi, color = x2), alpha = 0.3, fill = "white", linetype = "dashed") +
   geom_line(aes(color = x2), size = 1) +
   geom_errorbar(data = dat.plot.noshrubs, aes(x = x1, ymin = PSR.lo, ymax = PSR.hi), width = 0.01) +
   geom_point(data = dat.plot.noshrubs, aes(x = x1, y = PSR.md), size = 2) +
-  scale_color_manual(labels = c("0.02", "18"), values = c("#999999", "#009E73")) +
-  scale_fill_manual(labels = c("0.02", "18"), values = c("#999999", "#009E73")) + 
+  scale_color_manual(labels = c("0.02", "18"), values = c("#999999", "#D55E00")) +  #009E73
   annotate("text", x = -0.03, y = 0.3, label = "No shrubs", size = 4, angle = 90) +
   geom_vline(xintercept = 0.055, linetype = "dashed") +
-  labs(fill = "Shrub cover (%)", color = "Shrub cover (%)") + 
+  labs(color = "Shrub cover (%)") + 
   ylim(0, 0.35) +
   xlab("Max shrub height 5m (m)") + ylab("Survival over 90 days")
 
@@ -437,12 +485,19 @@ dat.plot <- dat.plot.Max_Shrub_Height_5mXShrub_All_5m.GRSP %>%
   mutate(precision = 1/(PSR.hi - PSR.lo)) %>%
   mutate(x2 = x2*100)
 p.heat.GRSP <- ggplot(dat.plot, aes(x1, x2)) +
-  geom_tile(aes(fill = PSR.md), color = "white") +
-  scale_fill_gradient(low = "#0072B2", high = "#D55E00") +
+  #geom_tile(aes(fill = PSR.md), color = "white") +
+  geom_raster(aes(fill = PSR.md)) +
+  geom_contour(aes(z = PSR.md),
+               color = "white", linetype = 2, size = 0.8, binwidth = 0.04) +
+#  scale_fill_gradient(low = "#0072B2", high = "#009E73", ##F0E442
+#                      breaks = seq(0, 0.22, by = 0.04)) +
+  scale_fill_gradient(low = "blue", high = "yellow", ##F0E442
+                        breaks = seq(0, 0.22, by = 0.04)) +
+  #scale_fill_gradient(colors = viridis(5)) +
   ylab("Shrub cover (%)") +
   xlab("Shrub height (m)") +
   theme(legend.title = element_text(size = 10),
-        legend.text = element_text(size = 12)) +
+        legend.text = element_text(size = 8)) +
   labs(fill = "Survival over\n90 days")
 
 # Put interactions together #
--- 03-Plot_SiteXSeason_estimates_abridged.R
+++ 03-Plot_SiteXSeason_estimates_abridged.R
@@ -15,69 +15,33 @@ scripts.loc <- "grass-surv-scripts/"
 ## Plot predictions ##
 
 spp <- "BAIS" # BAIS or GRSP
-mod.nam <- "BigCheese"
-mod <- loadObject(str_c("mod_mcmcR_", mod.nam, "_", spp))
-source(str_c(scripts.loc, "Data_processing_", mod.nam,".R"))
-dimnames(X)[[3]] <- X.nams
-
-nsims <- dim(mod$sims.concat)[2]
-nsims.samp <- sample(nsims, 5000)
-Site_Season <- str_c(data.spp$Covs$Site, data.spp$Covs$Season, sep = "_")
-X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
-X.sum <- X.sum %>% array(dim = c(dim(X.sum), length(nsims.samp))) %>% aperm(c(4, 1:3))
-npar <- dim(mod$sims.concat)[1]
-B <- mod$sims.concat[1:(npar-2),nsims.samp] %>% array(dim = dim(X.sum)[c(4, 1, 2, 3)]) %>% aperm(c(2, 3, 4, 1))
-DSR <- expit(apply(B * X.sum, c(1, 2, 3), sum))
-
+source(str_c(scripts.loc, "Data_processing_BigCheese.R"))
 st <- 1:(J-89)
 end <- 90:J
-PSR <- apply(DSR, c(1, 2), function(x) {
-  v <- numeric(length = length(st))
-  for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
-  return(mean(v))
-})
+source(str_c("grass-surv-scripts/Data_processing_JAGS.r"))
+dimnames(X)[[3]] <- X.nams
+modSS <- loadObject(str_c("mod_CJSRL_SiteXSeason_Transmitter", "_", spp))
+Site_Season <- str_c(data.spp$Covs$Site, data.spp$Covs$Season, sep = "_")
+X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
+X.sum <- X.sum[,,X.nams]
+SeasonRef <- data.spp$Covs %>% select(Season, SeasonInd) %>% unique %>% arrange(SeasonInd)
+SiteRef <- data.spp$Covs %>% select(Site, SiteInd) %>% unique %>% arrange(SiteInd)
+
 dat.plot <- SiteSeasons %>%
-  mutate(PSR.md = apply(PSR, 2, median),
-         PSR.lo = apply(PSR, 2, function(x) quantile(x, prob = 0.05)),
-         PSR.hi = apply(PSR, 2, function(x) quantile(x, prob = 0.95))) %>%
   left_join(
     data.spp$Covs %>% select(Site, Season) %>%
       dplyr::group_by(Site, Season) %>%
       summarise(n = n()),
     by = c("Site", "Season")
-  )
-
-## Compare with Site X Season estimates ###
-
-modSS <- loadObject(str_c("mod_CJSRL_SiteXSeason_Transmitter", "_", spp))
-X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
-source(str_c("grass-surv-scripts/Data_processing_JAGS.r"))
-X.sum <- X.sum[,,X.nams]
-SeasonRef <- data.spp$Covs %>% select(Season, SeasonInd) %>% unique %>% arrange(SeasonInd)
-SiteRef <- data.spp$Covs %>% select(Site, SiteInd) %>% unique %>% arrange(SiteInd)
-dat.plot <- dat.plot %>%
-  mutate(PSR.est.obsCovs = NA,
-         PSR.est.obsCovs.lo = NA,
-         PSR.est.obsCovs.hi = NA,
-         PSR.est.mnCovs = NA,
-         PSR.est.mnCovs.lo = NA,
-         PSR.est.mnCovs.hi = NA)
+  ) %>%
+  mutate(PSR.est = NA,
+         PSR.est.lo = NA,
+         PSR.est.hi = NA)
 
 for(j in 1:nrow(SiteRef)) for(t in 1:nrow(SeasonRef)) {
   row.ind <- which(dat.plot$Season == SeasonRef$Season[t] & dat.plot$Site == SiteRef$Site[j])
   if(length(row.ind) == 1) {
     B0 <- modSS$sims.list$B0[, SiteRef$SiteInd[j], SeasonRef$SeasonInd[t]]
-    DSR <- array(NA, dim = c(length(B0), J))
-    for(d in 1:J) DSR[,d] <- expit(B0 + apply(t(t(modSS$sims.list$B) * X.sum[row.ind,d,]), 1, sum))
-    PSR <- apply(DSR, 1, function(x) {
-      v <- numeric(length = length(st))
-      for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
-      return(mean(v))
-    })
-    dat.plot[row.ind, "PSR.est.obsCovs"] <- median(PSR)
-    dat.plot[row.ind, "PSR.est.obsCovs.lo"] <- quantile(PSR, probs = 0.05, type = 8)
-    dat.plot[row.ind, "PSR.est.obsCovs.hi"] <- quantile(PSR, probs = 0.95, type = 8)
-    
     DSR <- array(NA, dim = c(length(B0), J))
     for(d in 1:J) DSR[,d] <- expit(B0 + apply(t(t(modSS$sims.list$B[,1:2]) * X.sum[row.ind,d,1:2]), 1, sum))
     PSR <- apply(DSR, 1, function(x) {
@@ -85,9 +49,9 @@ for(j in 1:nrow(SiteRef)) for(t in 1:nrow(SeasonRef)) {
       for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
       return(mean(v))
     })
-    dat.plot[row.ind, "PSR.est.mnCovs"] <- median(PSR)
-    dat.plot[row.ind, "PSR.est.mnCovs.lo"] <- quantile(PSR, probs = 0.05, type = 8)
-    dat.plot[row.ind, "PSR.est.mnCovs.hi"] <- quantile(PSR, probs = 0.95, type = 8)
+    dat.plot[row.ind, "PSR.est"] <- median(PSR)
+    dat.plot[row.ind, "PSR.est.lo"] <- quantile(PSR, probs = 0.05, type = 8)
+    dat.plot[row.ind, "PSR.est.hi"] <- quantile(PSR, probs = 0.95, type = 8)
   }
 }
 
@@ -95,29 +59,12 @@ dat.plot <- dat.plot %>%
   mutate(Site = as.factor(factor(Site)),
          Season = as.factor(Season))
 
-p.BAIS.pred <- ggplot(dat.plot, aes(x = Season, y = PSR.md, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.lo, ymax = PSR.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.md), position = position_dodge(0.8), angle = 90) +
+p.BAIS <- ggplot(dat.plot, aes(x = Season, y = PSR.est, fill = Site, shape = Site)) +
+  geom_errorbar(aes(ymin = PSR.est.lo, ymax = PSR.est.hi), width = 0.2, position = position_dodge(0.8)) +
+  geom_label(aes(label = n, x = Season, y = PSR.est), position = position_dodge(0.8), angle = 90) +
   scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
   ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Regularized covariate model", hjust = 0)
-
-p.BAIS.est.obsCovs <- ggplot(dat.plot, aes(x = Season, y = PSR.est.obsCovs, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.est.obsCovs.lo, ymax = PSR.est.obsCovs.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.est.obsCovs), position = position_dodge(0.8), angle = 90) +
-  scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
-  ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Random effect model with varying covariates", hjust = 0)
-
-p.BAIS.est.mnCovs <- ggplot(dat.plot, aes(x = Season, y = PSR.est.mnCovs, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.est.mnCovs.lo, ymax = PSR.est.mnCovs.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.est.mnCovs), position = position_dodge(0.8), angle = 90) +
-  scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
-  ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Random effect model with fixed covariates", hjust = 0)
+  ylab(NULL) + xlab("Winter (Nov-Mar)")
 
 #######################
 # Grasshopper Sparrow #
@@ -125,70 +72,34 @@ p.BAIS.est.mnCovs <- ggplot(dat.plot, aes(x = Season, y = PSR.est.mnCovs, fill =
 
 ## Plot predictions ##
 
-spp <- "GRSP" # GRSP or GRSP
-mod.nam <- "BigCheese"
-mod <- loadObject(str_c("mod_mcmcR_", mod.nam, "_", spp))
-source(str_c(scripts.loc, "Data_processing_", mod.nam,".R"))
-dimnames(X)[[3]] <- X.nams
-
-nsims <- dim(mod$sims.concat)[2]
-nsims.samp <- sample(nsims, 5000)
-Site_Season <- str_c(data.spp$Covs$Site, data.spp$Covs$Season, sep = "_")
-X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
-X.sum <- X.sum %>% array(dim = c(dim(X.sum), length(nsims.samp))) %>% aperm(c(4, 1:3))
-npar <- dim(mod$sims.concat)[1]
-B <- mod$sims.concat[1:(npar-2),nsims.samp] %>% array(dim = dim(X.sum)[c(4, 1, 2, 3)]) %>% aperm(c(2, 3, 4, 1))
-DSR <- expit(apply(B * X.sum, c(1, 2, 3), sum))
-
+spp <- "GRSP" # BAIS or GRSP
+source(str_c(scripts.loc, "Data_processing_BigCheese.R"))
 st <- 1:(J-89)
 end <- 90:J
-PSR <- apply(DSR, c(1, 2), function(x) {
-  v <- numeric(length = length(st))
-  for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
-  return(mean(v))
-})
+source(str_c("grass-surv-scripts/Data_processing_JAGS.r"))
+dimnames(X)[[3]] <- X.nams
+modSS <- loadObject(str_c("mod_CJSRL_SiteXSeason_Transmitter", "_", spp))
+Site_Season <- str_c(data.spp$Covs$Site, data.spp$Covs$Season, sep = "_")
+X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
+X.sum <- X.sum[,,X.nams]
+SeasonRef <- data.spp$Covs %>% select(Season, SeasonInd) %>% unique %>% arrange(SeasonInd)
+SiteRef <- data.spp$Covs %>% select(Site, SiteInd) %>% unique %>% arrange(SiteInd)
+
 dat.plot <- SiteSeasons %>%
-  mutate(PSR.md = apply(PSR, 2, median),
-         PSR.lo = apply(PSR, 2, function(x) quantile(x, prob = 0.05)),
-         PSR.hi = apply(PSR, 2, function(x) quantile(x, prob = 0.95))) %>%
   left_join(
     data.spp$Covs %>% select(Site, Season) %>%
       dplyr::group_by(Site, Season) %>%
       summarise(n = n()),
     by = c("Site", "Season")
-  )
-
-## Compare with Site X Season estimates ###
-
-modSS <- loadObject(str_c("mod_CJSRL_SiteXSeason_Transmitter", "_", spp))
-X.sum <- X %>% apply(c(2, 3), function(x) tapply(x, Site_Season, mean))
-source(str_c("grass-surv-scripts/Data_processing_JAGS.r"))
-X.sum <- X.sum[,,X.nams]
-SeasonRef <- data.spp$Covs %>% select(Season, SeasonInd) %>% unique %>% arrange(SeasonInd)
-SiteRef <- data.spp$Covs %>% select(Site, SiteInd) %>% unique %>% arrange(SiteInd)
-dat.plot <- dat.plot %>%
-  mutate(PSR.est.obsCovs = NA,
-         PSR.est.obsCovs.lo = NA,
-         PSR.est.obsCovs.hi = NA,
-         PSR.est.mnCovs = NA,
-         PSR.est.mnCovs.lo = NA,
-         PSR.est.mnCovs.hi = NA)
+  ) %>%
+  mutate(PSR.est = NA,
+         PSR.est.lo = NA,
+         PSR.est.hi = NA)
 
 for(j in 1:nrow(SiteRef)) for(t in 1:nrow(SeasonRef)) {
   row.ind <- which(dat.plot$Season == SeasonRef$Season[t] & dat.plot$Site == SiteRef$Site[j])
   if(length(row.ind) == 1) {
     B0 <- modSS$sims.list$B0[, SiteRef$SiteInd[j], SeasonRef$SeasonInd[t]]
-    DSR <- array(NA, dim = c(length(B0), J))
-    for(d in 1:J) DSR[,d] <- expit(B0 + apply(t(t(modSS$sims.list$B) * X.sum[row.ind,d,]), 1, sum))
-    PSR <- apply(DSR, 1, function(x) {
-      v <- numeric(length = length(st))
-      for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
-      return(mean(v))
-    })
-    dat.plot[row.ind, "PSR.est.obsCovs"] <- median(PSR)
-    dat.plot[row.ind, "PSR.est.obsCovs.lo"] <- quantile(PSR, probs = 0.05, type = 8)
-    dat.plot[row.ind, "PSR.est.obsCovs.hi"] <- quantile(PSR, probs = 0.95, type = 8)
-    
     DSR <- array(NA, dim = c(length(B0), J))
     for(d in 1:J) DSR[,d] <- expit(B0 + apply(t(t(modSS$sims.list$B[,1:2]) * X.sum[row.ind,d,1:2]), 1, sum))
     PSR <- apply(DSR, 1, function(x) {
@@ -196,9 +107,9 @@ for(j in 1:nrow(SiteRef)) for(t in 1:nrow(SeasonRef)) {
       for(j in 1:length(st)) v[j] <- prod(x[st[j]:end[j]])
       return(mean(v))
     })
-    dat.plot[row.ind, "PSR.est.mnCovs"] <- median(PSR)
-    dat.plot[row.ind, "PSR.est.mnCovs.lo"] <- quantile(PSR, probs = 0.05, type = 8)
-    dat.plot[row.ind, "PSR.est.mnCovs.hi"] <- quantile(PSR, probs = 0.95, type = 8)
+    dat.plot[row.ind, "PSR.est"] <- median(PSR)
+    dat.plot[row.ind, "PSR.est.lo"] <- quantile(PSR, probs = 0.05, type = 8)
+    dat.plot[row.ind, "PSR.est.hi"] <- quantile(PSR, probs = 0.95, type = 8)
   }
 }
 
@@ -206,63 +117,19 @@ dat.plot <- dat.plot %>%
   mutate(Site = as.factor(factor(Site)),
          Season = as.factor(Season))
 
-p.GRSP.pred <- ggplot(dat.plot, aes(x = Season, y = PSR.md, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.lo, ymax = PSR.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.md), position = position_dodge(0.8), angle = 90) +
+p.GRSP <- ggplot(dat.plot, aes(x = Season, y = PSR.est, fill = Site, shape = Site)) +
+  geom_errorbar(aes(ymin = PSR.est.lo, ymax = PSR.est.hi), width = 0.2, position = position_dodge(0.8)) +
+  geom_label(aes(label = n, x = Season, y = PSR.est), position = position_dodge(0.8), angle = 90) +
   scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
   ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Regularized covariate model", hjust = 0)
-
-p.GRSP.est.obsCovs <- ggplot(dat.plot, aes(x = Season, y = PSR.est.obsCovs, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.est.obsCovs.lo, ymax = PSR.est.obsCovs.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.est.obsCovs), position = position_dodge(0.8), angle = 90) +
-  scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
-  ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Random effect model with varying covariates", hjust = 0)
-
-p.GRSP.est.mnCovs <- ggplot(dat.plot, aes(x = Season, y = PSR.est.mnCovs, fill = Site, shape = Site)) +
-  geom_errorbar(aes(ymin = PSR.est.mnCovs.lo, ymax = PSR.est.mnCovs.hi), width = 0.2, position = position_dodge(0.8)) +
-  geom_label(aes(label = n, x = Season, y = PSR.est.mnCovs), position = position_dodge(0.8), angle = 90) +
-  scale_discrete_manual(aesthetics = "fill", labels = c("Cuza", "Janos", "Marfa", "Vaco"), values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442")) +
-  ylim(0,1) +
-  ylab(NULL) + xlab("Winter (Nov-Mar)") +
-  annotate("text", x = 1, y = 1, label = "Random effect model with fixed covariates", hjust = 0)
+  ylab(NULL) + xlab("Winter (Nov-Mar)")
 
 p <- ggdraw() +
-  draw_plot(p.BAIS.pred,        x = 0.05,  y = 0.6333334, width = 0.475,  height = 0.3166667) +
-  draw_plot(p.GRSP.pred,        x = 0.525, y = 0.6333334, width = 0.475,  height = 0.3166667) +
-  draw_plot(p.BAIS.est.obsCovs, x = 0.05,  y = 0.3166667, width = 0.475,  height = 0.3166667) +
-  draw_plot(p.GRSP.est.obsCovs, x = 0.525, y = 0.3166667, width = 0.475,  height = 0.3166667) +
-  draw_plot(p.BAIS.est.mnCovs,  x = 0.05,  y = 0,         width = 0.475,  height = 0.3166667) +
-  draw_plot(p.GRSP.est.mnCovs,  x = 0.525, y = 0,         width = 0.475,  height = 0.3166667) +
+  draw_plot(p.BAIS, x = 0.05,  y = 0, width = 0.475, height = 0.95) +
+  draw_plot(p.GRSP, x = 0.525, y = 0, width = 0.475, height = 0.95) +
   draw_plot_label(c("Baird's Sparrow", "Grasshopper Sparrow", "Survival over 90 days"),
                   x = c(0.15, 0.65, 0), y = c(0.97, 0.97, 0.5), size = c(15, 15, 25),
                   hjust = c(0.5, 0.5, 0.5), angle = c(0, 0, 90))
 
-save_plot("Figure_SiteXYear_Rates.jpg", p, ncol = 3.8, nrow = 4.3, dpi = 600)
+save_plot("Figure_SiteXYear_Rates_abridged.jpg", p, ncol = 3.8, nrow = 1.5, dpi = 600)
 
-## Abandoned script for summarizing z's to get SiteXSeason estimates ##
-# dat.plot <- dat.plot %>%
-#   mutate(ESR = 0,
-#          ESR.lo = 0,
-#          ESR.hi = 0)
-# 
-# Site <- str_split(Site_Season, "_", simplify = T)[,1]
-# Season <- str_split(Site_Season, "_", simplify = T)[,2]
-# 
-# for(i in 1:nrow(dat.plot)) {
-#   ind.SiteSeason <- which(Site == dat.plot$Site[i] & Season == dat.plot$Season[i])
-#   n.survived <- n.mortality <- array(NA, dim = c(dim(modSS$sims.list$z)[1], length(ind.SiteSeason)))
-#   for(j in 1:length(ind.SiteSeason)) {
-#     z <- modSS$sims.list$z[, ind.SiteSeason[j], (first[ind.SiteSeason[j]]+1):last[ind.SiteSeason[j]]]
-#     n.survived[,j] <- ifelse(is.matrix(z), apply(z, 1, sum), z)
-#     n.mortality[,j] <- ifelse(is.matrix(z), apply(z, 1, function(x) any(x == 0)*1), (z == 0) * 1)
-#   }
-#   DSR <- apply(n.survived, 1, sum) / apply(n.survived + n.mortality, 1, sum)
-#   PSR <- DSR^90
-#   dat.plot[i, "ESR"] <- median(PSR)
-#   dat.plot[i, "ESR.lo"] <- quantile(PSR, prob = 0.05, type = 8)
-#   dat.plot[i, "ESR.hi"] <- quantile(PSR, prob = 0.95, type = 8)
-# }
